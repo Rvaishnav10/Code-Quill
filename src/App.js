3c1114437298a3126ef4f10c8ccbb6779a0b9a89
Rvaishnav10 <vaishnavritu761@gmail.com>
2023-10-21 18:31:59 +0530

    updated to 2023

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
+import {Home, NewProject } from './container'
+import { auth, db } from './config/firebase.config'
+import { doc, onSnapshot, orderBy, query, collection, setDoc } from 'firebase/firestore'
+import { Spinner } from './components'
+import { useDispatch } from 'react-redux'
+import { SET_USER } from './context/actions/userActions'
+import { SET_PROJECTS } from './context/actions/projectActions'
 
-function App() {
+
+const App = () => {
+
+const navigate = useNavigate();
+const [isLoading, setIsLoading] = useState(true);
+const dispatch = useDispatch()
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(userCred => {
+            if(userCred){
+                 console.log(userCred?.providerData[0]);
+                 setDoc(doc(db, "users", userCred?.uid), userCred?.providerData[0]).then(() => {
+                  //dispatch the action to store
+                     dispatch(SET_USER(userCred?.providerData[0]));
+                     navigate("/home/projects", {replace : true});
+                 })
+            } else {
+                  navigate("/home/auth", {replace : true})
+            }
+
+            setInterval(() => {
+              setIsLoading(false)
+            }, 2000);
+        });
+
+        //cleanup the the listener event
+        return () => unsubscribe();
+    }, []);
+
+    useEffect(() => {
+      const projectQuery = query(
+        collection(db, "Projects"),
+        orderBy("id", "desc")
+      )
+      const unsubscribe = onSnapshot(projectQuery, (querySnaps => {
+        const projectsList = querySnaps.docs.map(doc => doc.data())
+        dispatch(SET_PROJECTS(projectsList))
+      }))
+
+      return unsubscribe;
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {isLoading ? (
+    <div className='w-screen h-screen flex items-center justify-center overflow-hidden'>
+    <Spinner />
+
+    </div>) : (
+      <div className='w-screen h-screen flex items-start justify-start overflow-hidden'>
+      <Routes>
+        <Route path="/home/*" element={<Home />} />
+        <Route path="/newProject" element={<NewProject />}/>
+
+        {/* if the route not match like we want localhost:3000/home/* home but it always redirect to localhost:3000 then we need to reroute the user */}
+        <Route path="*" element={<Navigate to={"/Home"} />} />
+      </Routes>
     </div>
-  );
+    )}
+    
+    
+    
+    
+    </>
+  )
 }
 
-export default App;
+export default App
